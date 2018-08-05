f738264801d2b84c624df4afede960a0c95438b4
Kostas Livieratos <koslibpro@gmail.com>
2018-08-05 22:26:45 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pyopenexrates',
+    description='An openexchangerates.org service API wrapper for Python',
+    version='0.1',
+    url='https://github.com/intelligems/pyopenexrates',
+    author='K. Livieratos',
+    author_email='kostas@intelligems.eu',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['pyopenexrates'],
+    install_requires=[
+        'requests>=2.19.1'
+    ]
+)
